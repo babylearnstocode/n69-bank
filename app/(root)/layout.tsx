d2a627e0db7d4cebb8a0e4331b7a7584246536ed
tommy <huyanh2504@gmail.com>
2025-05-10 12:02:50 +0200

    add route, layout

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+      SIDEBAR
+      {children}
+    </main>
+  );
+}
